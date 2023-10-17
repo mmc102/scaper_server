130450444f008edde60c1314486521c504f24de9
Matthew Carroll <60335800+mmc102@users.noreply.github.com>
2023-10-17 11:09:47 -0700

    add kill

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Request, Form
+from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
@@ -12,11 +13,30 @@ async def read_root(request: Request):
         lines = file.readlines()
     return templates.TemplateResponse("form.html", {"request": request, "lines":lines})
 
-@app.post("/")
+@app.post("/add")
 async def write_dates(request: Request, name:str=Form(...), start_date: str = Form(...), end_date: str = Form(...)):
     with open("dates.txt", "a") as file:
         file.write(f"{name}|{start_date}|{end_date}\n")
+
     return templates.TemplateResponse("form.html", {"request": request})
+
+@app.post("/remove")
+async def remove_line(request:Request,line: str= Form(...)):
+    # Remove the line from the list
+    with open("dates.txt", "r") as file:
+        lines = file.readlines()
+    try:
+        lines.remove(line.replace('\r',""))
+    except ValueError:
+        pass
+
+    # Update the file with the new lines
+    with open("dates.txt", "w") as file:
+        file.writelines(lines)
+
+
+    return templates.TemplateResponse("form.html", {"request": request})
+
 
 if __name__ == "__main__":
     import uvicorn
